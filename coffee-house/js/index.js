7efb005029dd2b7f53f227423cc8ef2e4dc2f6ee
k98940 <pv1xxb@yandex.ru>
2023-11-21 20:28:43 +0600

    feat: added cards in menu. (Tue, Nov 21, 2023 08:28:43 PM)

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -1,4 +1,5 @@
+
 // window.scrollTo(0, document.body.scrollHeight);
-const scroll = document.body.querySelector('.mobileapp-btn-container')
-console.log('scroll to :>> ', scroll);
+// const scroll = document.body.querySelector('.mobileapp-btn-container')
+// console.log('scroll to :>> ', scroll);
 // scroll.scrollIntoView();
