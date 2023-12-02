36f32b70a87c3110daba7313e94cbbf75c8ad67f
k98940 <pv1xxb@yandex.ru>
2023-12-02 09:16:13 +0600

    feat: add support click event for slider. (Sat, Dec 02, 2023 09:16:13 AM)

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -1,13 +1,28 @@
 export const initSlider = () => {
+  const DIRECTION_SLIDE = {
+    'left': 1,
+    'rigth': -1,
+  };
   const dots = document.querySelectorAll('.slider-dots-item');
   const slider = document.querySelector('.slider');
+  const btnLeft = document.querySelector('.slider-btn-left');
+  const btnRight = document.querySelector('.slider-btn-right');
   const countSlides = dots.length;
 
-  const animationEndHandle = (e) => {
-    const currentSlide = parseInt(e.target.parentElement.attributes.slide.value);
-    let nextSlide = currentSlide + 1 > countSlides
-      ? 1
-      : currentSlide + 1;
+  const doSlide = (_, direction = DIRECTION_SLIDE.left) => {
+    const pointer = document.querySelector('.slider-dots-item[pointer]');
+    const currentSlide = parseInt(pointer.attributes.slide.value);
+    const getIndexNextSlide = () => {
+      let index = currentSlide + direction;
+      if (index > countSlides) {
+        index = 1;
+      } else if (index < 1) {
+        index = countSlides;
+      }
+      return index;
+    }
+
+    const nextSlide = getIndexNextSlide();
     const nextDot = document.querySelector(`[slide="${nextSlide}"]`);
     const currDot = document.querySelector(`[slide="${currentSlide}"]`);
 
@@ -16,5 +31,11 @@ export const initSlider = () => {
     nextDot.setAttribute('pointer', '');
   }
 
-  dots.forEach(dot => { dot.addEventListener('animationend', animationEndHandle) });
+  try {
+    dots.forEach(dot => { dot.addEventListener('animationend', (e) => doSlide(e)) });
+    btnLeft.addEventListener('click', (e) => doSlide(e, DIRECTION_SLIDE.left));
+    btnRight.addEventListener('click', (e) => doSlide(e, DIRECTION_SLIDE.rigth));
+  } catch (error) {
+    console.error(error);
+  }
 }
