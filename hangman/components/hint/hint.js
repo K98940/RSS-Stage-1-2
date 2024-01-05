a2694820c48477eb371bf3fed66415d3531e8b09
k98940 <pv1xxb@yandex.ru>
2024-01-05 21:28:01 +0600

    feat: add some words for questions. Add component Hint.

--- hangman/components/hint/hint.js
+++ hangman/components/hint/hint.js
@@ -1,9 +1,11 @@
 import { createTag } from '../../utils/createTag.js';
+import { state } from '../../utils/loadConfig.js';
 
 export const hint = () => {
-  const hint = createTag('div');
-  hint.classList = 'hint';
-  hint.innerText = 'hint';
+  const { hint } = state.newQuestion;
+  const hintElement = createTag('div');
+  hintElement.classList = 'hint';
+  hintElement.innerText = hint;
 
-  return hint;
+  return hintElement;
 };
