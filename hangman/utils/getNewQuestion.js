0505af36f3bfe97b661aa633051362a7cf53e561
k98940 <pv1xxb@yandex.ru>
2024-01-08 19:36:39 +0600

    feat: add loop for the secret words

--- hangman/utils/getNewQuestion.js
+++ hangman/utils/getNewQuestion.js
@@ -1,18 +1,19 @@
-import { createModal } from '../components/modal/modal.js';
 import { state } from './loadConfig.js';
 
 export const getNewQuestion = () => {
-  const { questions } = state;
-  const count = questions?.length || 0;
+  const count = state.questions?.length || 0;
   if (count === 0) {
-    console.warn('empty list of the words');
-    return;
+    console.warn('The word list is over. New loop.');
+    state.questions = state.questionsPast;
+    state.questionsPast = [];
   }
-  const RandomIndex = Math.floor(Math.random() * count);
-  return questions[RandomIndex];
-};
 
-// TODO: if count === 0 => createModal('empty list of the words')
-// else
-//    wordPast = splice(questions[RandomIndex], 1)
-//    обновить UI counters (questions remain and correct answers)
+  const RandomIndex = Math.floor(Math.random() * count);
+  const secretWord = state.questions.splice(RandomIndex, 1)[0];
+  state.questionsPast.push(secretWord);
+
+  console.warn('RandomIndex', RandomIndex);
+  console.warn('secretWord', secretWord);
+
+  return secretWord;
+};
