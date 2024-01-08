85cc93f1c0b480a3acac8f11458a13505dc9f030
k98940 <pv1xxb@yandex.ru>
2024-01-08 21:31:56 +0600

    refactor: initGame take delay value from config

--- hangman/utils/initGame.js
+++ hangman/utils/initGame.js
@@ -13,6 +13,7 @@ export const initGame = async () => {
 
 const newRound = () => {
   return new Promise((resolve, _) => {
+    const { delay } = state;
     const keys = document.querySelectorAll(
       '.virtual-keyboard__key[data-loose], .virtual-keyboard__key[data-succes]'
     );
@@ -21,24 +22,24 @@ const newRound = () => {
         setTimeout(() => {
           key.removeAttribute('data-loose');
           key.removeAttribute('data-succes');
-        }, 200 * (i + 1));
+        }, delay * (i + 1));
       });
 
     const gallows = document.querySelector('.gallows');
     gallows &&
       state.errorOrder.forEach((error, i) =>
-        setTimeout(() => gallows.removeAttribute(error), 200 * (i + 1))
+        setTimeout(() => gallows.removeAttribute(error), delay * (i + 1))
       );
 
     const characters = document.querySelectorAll('.question__character');
     characters &&
       characters.forEach((char, i, chars) => {
-        setTimeout(() => char.removeAttribute('data-pressed'), 200 * (i + 1));
+        setTimeout(() => char.removeAttribute('data-pressed'), delay * (i + 1));
         if (i + 1 === chars.length)
           setTimeout(() => {
             word();
             hint();
-          }, 2000);
+          }, delay * (i + 1) + 1000);
       });
 
     const incorrectGuesses = document.querySelector(
@@ -48,7 +49,7 @@ const newRound = () => {
       for (let i = state.incorrectGuesses; i >= 0; i -= 1) {
         setTimeout(() => {
           incorrectGuesses.innerText = `${i} / ${state.errorLimit}`;
-        }, 250 * (state.errorLimit - i + 1));
+        }, delay * (state.errorLimit - i + 1));
         state.incorrectGuesses = i;
       }
     }
