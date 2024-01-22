19df9f7e46a73b60ed6798b662125314e0e29c91
k98940 <pv1xxb@yandex.ru>
2024-01-22 20:22:04 +0600

    feat: add listener for mouse click. Add check for game over (win)

--- nonograms/src/components/plotContainer/plotContainer.js
+++ nonograms/src/components/plotContainer/plotContainer.js
@@ -7,12 +7,49 @@ export default () => {
   const { plot } = state.game;
   const container = createElement({ tag: 'div', cls: 'container' });
   console.log(plot);
-  plot.forEach((row) =>
-    row.forEach((el) => {
+  plot.forEach((row, i) =>
+    row.forEach((el, j) => {
       const cell = newCell();
+      plot[i][j] = {
+        state: '⚪',
+        value: el,
+        element: cell,
+      };
       container.appendChild(cell);
     })
   );
 
+  container.addEventListener('click', plotHandler);
   return container;
+};
+
+const plotHandler = (e) => {
+  const { plot } = state.game;
+  const clickedCell = plot
+    .flat(1)
+    .filter((cell) => cell.element === e.target)[0];
+
+  if (clickedCell) {
+    const { state } = clickedCell;
+    switch (state) {
+      case '⚪':
+        clickedCell.state = '⚫';
+        clickedCell.element.classList.add('cell_fill');
+        break;
+      case '⚫':
+        clickedCell.state = '⚪';
+        clickedCell.element.classList.remove('cell_fill');
+        break;
+
+      default:
+        break;
+    }
+
+    if (isAllCorrectChecked()) setTimeout(() => alert('you win'), 0);
+  }
+};
+
+const isAllCorrectChecked = () => {
+  const { plot } = state.game;
+  return !plot.flat(1).some((cell) => cell.state !== cell.value);
 };
