02bb2fdd958b5876b3e61bca3e106581a364d18b
k98940 <pv1xxb@yandex.ru>
2024-02-02 10:41:13 +0600

    fix: fix welcome message (was a problem with light theme console)

--- nonograms/src/utils/showWelcome.js
+++ nonograms/src/utils/showWelcome.js
@@ -1,29 +1,26 @@
 export default () => {
   console.clear();
   console.log(
-    `%cПривет!
-%cДля облегчения проверки конечно же есть кое-то.
-клавиша %c H %c(hide) покажет / скроет подсказку`,
-    'background-color: #000000; color: #ffff00',
-    'background-color: #000000',
-    'background-color: #000000; color: #ffff00; font-size: 20px',
-    'background-color: #000000; color: #ffffff; font-size: 12px'
+    'Привет!%cДля облегчения проверки конечно же есть кое-что. Клавиша\n %c H \n%cнажатая на клавиатуре покажет / скроет подсказку',
+    'background-color: transparent; padding: 8px 4px',
+    'background-color: #555555; color: #ffff00; line-height: 26px; font-size: 20px',
+    'background-color: transparent; padding: 8px 4px; font-size: 12px'
   );
 
   console.log(
-    'Игровое поле:\n%cЛКМ:%c закрасить клетку\n%cПКМ:%c поставить крестик\nкнопка %cswitch brush:%c  переключить режим ЛКМ закрасить / поставить крестик\n',
+    'Игровое поле:\n%c ЛКМ: %c закрасить клетку\n%c ПКМ: %c поставить крестик\nкнопка %c switch brush: %c  переключить режим ЛКМ закрасить / поставить крестик (удобно пользоваться если нет мышки)\n',
     'background-color: #555555; color: #ffffff',
-    'background-color: transparent; color: #ffffff',
+    'background-color: transparent',
     'background-color: #555555; color: #ffffff',
-    'background-color: transparent; color: #ffffff',
+    'background-color: transparent',
     'background-color: #555555; color: #ffffff',
-    'background-color: transparent; color: #ffffff'
+    'background-color: transparent'
   );
   console.log(
-    'Поле с подсказками:\n%cЛКМ:%c заполнить крестиками всю строку / столбец\n%cПКМ:%c убрать все крестики на строке / столбце\n',
+    'Поле с подсказками:\n%c ЛКМ: %c заполнить крестиками всю строку / столбец\n%c ПКМ: %c убрать все крестики на строке / столбце\n',
     'background-color: #555555; color: #ffffff',
-    'background-color: transparent; color: #ffffff',
+    'background-color: transparent',
     'background-color: #555555; color: #ffffff',
-    'background-color: transparent; color: #ffffff'
+    'background-color: transparent'
   );
 };
