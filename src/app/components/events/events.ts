30364d78ea939fc199e0394860f9bb07ead7f177
Konstantin <88248839+K98940@users.noreply.github.com>
2024-04-14 13:59:53 +0600

    feat: implement EventEmmiter class (#90)

---
+++ src/app/components/events/events.ts
@@ -0,0 +1,22 @@
+type Handler = {
+  (...args: unknown[]): void;
+};
+type Handlers = {
+  [index: string]: Handler[];
+};
+
+export class EventEmmiter {
+  private handlers: Handlers = {};
+
+  protected on(event: string, handler: Handler): void {
+    if (!(event in this.handlers)) {
+      this.handlers[event] = [];
+    }
+    this.handlers[event].push(handler);
+  }
+
+  protected event(eventName: string, ...arg: unknown[]): void {
+    if (!(eventName in this.handlers)) return;
+    this.handlers[eventName].forEach((handler) => handler(...arg));
+  }
+}
